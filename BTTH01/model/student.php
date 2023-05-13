fb756a6cedbb0ddd0fedf0e69e82688b3ae5ef14
phamthao2002 <pnpthao2002@gmail.com>
2023-05-13 22:08:51 +0700

    Update code student.php

--- BTTH01/model/student.php
+++ BTTH01/model/student.php
@@ -11,4 +11,35 @@ class Student {
     $this->age = $age;
     $this->grade = $grade;
   }
+public function getId() {
+    return $this->id;
+  }
+
+  public function setId($id) {
+    $this->id = $id;
+  }
+
+  public function getName() {
+    return $this->name;
+  }
+
+  public function setName($name) {
+    $this->name = $name;
+  }
+
+  public function getAge() {
+    return $this->age;
+  }
+
+  public function setAge($age) {
+    $this->age = $age;
+  }
+
+  public function getGrade() {
+    return $this->grade;
+  }
+
+  public function setGrade($grade) {
+    $this->grade = $grade;
+  }
 }
